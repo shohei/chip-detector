7078e09a4f034c2c2a59edbd8972cb01037e7d24
Shohei Aoki <shoaok@gmail.com>
2017-05-02 17:55:45 +0900

    commit

--- masker.py
+++ masker.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from pylab import *
 
 image = cv2.imread('chip.png',cv2.CV_8UC1)
 image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 45, 0)
@@ -6,5 +8,27 @@ se = np.ones((7,7), dtype='uint8')
 image_close = cv2.morphologyEx(image, cv2.MORPH_CLOSE, se)
 cnt = cv2.findContours(image_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
 mask = np.zeros(image.shape[:2], np.uint8)
-cv2.drawContours(mask, cnt, -1, 255, -1)
-cv2.imshow("Keypoints", mask)
+
+
+cntr=0
+for cs in cnt:
+    print len(cs)
+    xs=[]
+    ys=[]
+    for c in cs:
+        if(cntr==4):
+            x = c[:,0][0]
+            y = c[:,1][0]
+            xs.append(x)
+            ys.append(y)
+            print x,y
+            plot(x,y,'ro')
+    cntr = cntr+1
+
+imshow(image,'gray')
+show()
+
+
+# cv2.drawContours(mask, cnt, -1, 255, -1)
+# cv2.imshow("Keypoints", mask)
+# cv2.waitKey(0)
