0d22f8ec9fe5918f4c2d523a4121bcd5190816ee
Shohei Aoki <shoaok@gmail.com>
2017-05-13 15:03:36 +0900

    temporal commit

--- em/em.py
+++ em/em.py
@@ -5,6 +5,10 @@ import math
 from numpy import dot
 from numpy.linalg import inv
 from numpy.linalg import det 
+import random
+from numpy.matlib import repmat
+from numpy import matrix
+import pdb
 
 iris = datasets.load_iris()
 data = iris.data
@@ -14,67 +18,78 @@ d = 4 #特徴量の次元
 K = 3 #クラス数
 
 def Gaussian(x, mu,sigma):
-    N = 1/((2*math.pi)**(1/K))*(det(sigma))**0.5*exp(-(1/2.0)*dot((dot((x-mu).T,inv(sigma))),(x-mu)))
+    coef = 1/(((2*math.pi)**(1/K))*(det(sigma))**0.5)
+    exp = math.e**(-(1/2.0)*dot((dot(matrix(x-mu),inv(sigma))),matrix(x-mu).T).item())
+    N = coef * exp
     return N
 
 """initialize"""
 gamma = np.zeros((len(data),K))
-for g in gamma:
-    g0 = random.random()
-    g1 = random.random(g0) 
-    g2 = 1-g0-g1
-    g = np.array([g1,g2,g3])
+for i in range(len(gamma)):
+    problist = [5/10.0, 4/10.0, 1/10.0]
+    rindex = int(math.floor(random.random()*3))
+    g0 = problist.pop(rindex)
+    rindex = int(math.floor(random.random()*2))
+    g1 = problist.pop(rindex)
+    g2 = problist[0] 
+    gamma[i][0] = g0
+    gamma[i][1] = g1
+    gamma[i][2] = g2
 
 pi = np.array([1/3.0,1/3.0,1/3.0]) #混合比
 
+labels = np.array([g.index(max(g)) for g in gamma.tolist()])
+
 """E-step"""
-idx_0 = np.where(gamma==0)
+idx_0 = np.where(labels==0)[0]
 x_0 = data[idx_0,:]
-mu_0 = sum(x_0) / len(x_0) 
-#mu_0s = np.zeros((len(x_0),1))*mu_0
+mu_0 = sum(x_0) / len(x_0)
+mu_0s = repmat(mu_0,len(x_0),1)
 S0 = np.zeros((d,d))
 for i in range(d):
     for j in range(d):
-        S0[i,j] = sum(dot((x_0[:,i]-mu_0[:,i]),(x_0[:,j]-mu_0[:,j])))/len(x_0)
+        S0[i,j] = sum((x_0[:,i]-mu_0s[:,i])*(x_0[:,j]-mu_0s[:,j]))/len(x_0)
 
-idx_1 = np.where(gamma==1)
+idx_1 = np.where(labels==1)[0]
 x_1 = data[idx_1,:]
-mu_1 = sum(x_1) / len(x_1) 
-#mu_1s = np.zeros((len(x_1),1))*mu_1
+mu_1 = sum(x_1) / len(x_1)
+mu_1s = repmat(mu_1,len(x_1),1)
 S1 = np.zeros((d,d))
 for i in range(d):
     for j in range(d):
-        S1[i,j] = sum(dot((x_1[:,i]-mu_1[:,i]),(x_1[:,j]-mu_1[:,j])))/len(x_1)
+        S1[i,j] = sum((x_1[:,i]-mu_1s[:,i])*(x_1[:,j]-mu_1s[:,j]))/len(x_1)
 
-idx_2 = np.where(gamma==2)
+idx_2 = np.where(labels==2)[0]
 x_2 = data[idx_2,:]
-mu_2 = sum(x_2) / len(x_2) 
-#mu_2s = np.zeros((len(x_2),1))*mu_2
+mu_2 = sum(x_2) / len(x_2)
+mu_2s = repmat(mu_2,len(x_2),1)
 S2 = np.zeros((d,d))
 for i in range(d):
     for j in range(d):
-        S2[i,j] = sum(dot((x_2[:,i]-mu_2[:,i]),(x_2[:,j]-mu_2[:,j])))/len(x_2)
-
+        S2[i,j] = sum((x_2[:,i]-mu_2s[:,i])*(x_2[:,j]-mu_2s[:,j]))/len(x_2)
 
 for i in range(len(data)):
-    xi = x[i,:]
-    gaussian_array = [Gaussian(xi,mu_0,S0),Gaussian(xi,mu_1,S1),Gaussian(xi,mu_2,S2)]
+    xi = data[i,:]
+    Gauss0 = Gaussian(xi,mu_0,S0)
+    Gauss1 = Gaussian(xi,mu_1,S1)
+    Gauss2 = Gaussian(xi,mu_2,S2)
+    gaussian_array = np.array([Gauss0,Gauss1,Gauss2])
     for k in range(K):
-        gamma[i][k] = (pi[k]*gaussian_array[k])/sum(dot(pi,gaussian_array))
+        gamma[i][k] = (pi[k]*gaussian_array[k])/sum(pi*gaussian_array)
 
 """M-step"""
-N0 = sum((np.matrix(gamma)[:,0]))
-N1 = sum((np.matrix(gamma)[:,1]))
-N2 = sum((np.matrix(gamma)[:,2]))
+N0 = sum(gamma[:,0]).item()
+N1 = sum(gamma[:,1]).item()
+N2 = sum(gamma[:,2]).item()
 
-mu_0 = 1/N0 * sum((np.matrix(gamma)[:,0])*x_0)
-mu_1 = 1/N1 * sum((np.matrix(gamma)[:,1])*x_1)
-mu_2 = 1/N2 * sum((np.matrix(gamma)[:,2])*x_2)
-mu = np.matrix([mu_0,mu_1,mu_2])
+mu_0  = 1/N0 * sum([gamma[i,0].item()*data[i,:] for i in range(len(data))])
+mu_1  = 1/N1 * sum([gamma[i,1].item()*data[i,:] for i in range(len(data))])
+mu_2  = 1/N2 * sum([gamma[i,2].item()*data[i,:] for i in range(len(data))])
+mu = matrix([mu_0,mu_1,mu_2])
 
-S_0 =  sum(dot(dot((np.matrix(gamma)[:,0]),(x_0-mu_0)),(x_0-mu_0).T))/N0
-S_1 =  sum(dot(dot((np.matrix(gamma)[:,1]),(x_1-mu_1)),(x_1-mu_1).T))/N1
-S_2 =  sum(dot(dot((np.matrix(gamma)[:,2]),(x_2-mu_2)),(x_2-mu_2).T))/N2
+S_0 =  sum(dot(dot(gamma[:,0],(x_0-mu_0)),(x_0-mu_0).T))/N0
+S_1 =  sum(dot(dot(gamma[:,1],(x_1-mu_1)),(x_1-mu_1).T))/N1
+S_2 =  sum(dot(dot(gamma[:,2],(x_2-mu_2)),(x_2-mu_2).T))/N2
 S = [S_0,S_1,S_2]
 
 N = N0+N1+N2
@@ -95,7 +110,7 @@ for i in range(len(gamma)):
 L = 0
 for i in range(len(zs)):
     for k in range(K):
-        L = L + zs[i][k]*math.log(pi[k]) + zs[i][k]*(-d/2*math.log(2*math.pi)_- 1/2.0*math.log(det(S[k])) - 1/2.0*(dot(dot((x[i,k]-mu[k]).T,inv(S[k])),(x[i,k]-mu[k]))))
+        L = L + zs[i][k]*math.log(pi[k]) + zs[i][k]*(-d/2*math.log(2*math.pi)- 1/2.0*math.log(det(S[k])) - 1/2.0*(dot(dot((x[i,k]-mu[k]).T,inv(S[k])),(x[i,k]-mu[k]))))
 
 
 
