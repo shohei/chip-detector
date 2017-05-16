19357d475ff49db7b9b04af26c5d079285092818
Shohei Aoki <shoaok@gmail.com>
2017-05-16 12:44:03 +0900

    temporal commit

--- neural/nn.py
+++ neural/nn.py
@@ -2,18 +2,22 @@
 import numpy as np
 from sklearn import datasets
 import math
+import random
+from numpy import matrix as mat
+import pdb
 
 iris = datasets.load_iris()
 data = iris.data
 target = iris.target
 
-I = 4 # Sepal width/height, Petal width/height
-J = 4 # Arbitrary
+I = 4+1 # Sepal width/height, Petal width/height (+ bias)
+J = 4+1 # Arbitrary (+ bias)
 K = 3 # Setosa, Virginica, Versicolor
 
 eta = 0.01
+beta = 1
 
-def sigmoid(x):
+def sigmoid(x): # x can be a vector
     return 1/(1-math.e**(-beta*x))
 
 def sigmoid_dash(x):
@@ -28,26 +32,55 @@ def softmax_dash(xi,xk,is_same_i_j):
     else:
         return -softmax(xi) * softmax(xk)
 
-# 出力層-隠れ層の計算
-hk=[]
-delta_k=[]
-d_wkj=[]
-for k in range(K):
+wkj = np.zeros((K,J))
+wji = np.zeros((J,I))
+def initialize():
+    for k in range(K):
+        for j in range(J):
+            wkj[k][j]  = random.random() 
     for j in range(J):
-        hk[k] =  sum(wkj[k][j] * softmax(sum(wji[j][i] * xk[k]))) 
-        delta_k[k] = (tk[k] - ok[k])*softmax_dash(hk[k]) 
-        d_wkj[k][j] = eta * sum(delta_k[k]) * Vj[j] 
+        for i in range(I):
+            wji[j][i]  = random.random() 
+initialize()
 
-# 隠れ層-入力層を計算
-hj=[]
-delta_j=[]
-d_wji=[]
-for j in range(J):
-    for i in range(I):
-        hj[j] =  sum(wji * xi) 
-        delta_j[j] =  softmax_dash(hj) * sum(delta_k * wkj) 
-        d_wji[j][i] =  eta * sum(delta_j * xk) 
+hj=np.zeros(J)
+hk=np.zeros(K)
+Vj=np.zeros(J)
+ok=np.zeros(K)
+def forward(xi):
+    for j in range(J):
+        hj[j] =  (np.dot(mat(wji[j,:]),mat(xi).T)).item() # scalar
+        Vj[j] = sigmoid(hj[j])
+    for k in range(K):
+        _hk = sum(np.dot(mat(wkj[k,:]),mat(Vj).T))
+        hk[k] = _hk.item() 
+    for k in range(K):
+        ok[k] = softmax(hk[k].item(),hk)
 
+delta_k=np.zeros((K,1))
+delta_j=np.zeros((J,1))
+d_wkj=np.zeros((K,J)) #JからKへの重み
+d_wji=np.zeros((J,I))
+def backpropagation(ti):
+    for k in range(K):
+        delta_k[k] = (ti[k] - ok[k])*softmax_dash(hk[k])
+        for j in range(J):
+            d_wkj[k][j] = eta * delta_k[k] * Vj[j]
+            wkj[k][j] = wkj[k][j] + d_wkj[k][j]
+    for J in range(J):
+        delta_j[j] = sigmoid_dash(hj[j]) * np.dot(mat(delta_k).T,mat(wkj[:,j]).T)
+        for i in range(I):
+            d_wji[j][i] = eta * delta_j[j] * xi[i]
+            wji[j][i] = wji[j][i] + d_wji[j][i]
 
+def makeTarget(t):
+    ti=[0,0,0]
+    ti[t] = 1 # t=0,1,2
+    return ti
 
+for i in range(len(data)):
+    xi = np.concatenate((np.array([1]),data[i,:]))
+    forward(xi)
+    ti = makeTarget(target[i])
+    backpropagation(ti)
 
